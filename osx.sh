e2c355164c890455f33ebbc3054da05ad7ef3f98
Spencer Chan <spencer@quora.com>
2014-08-02 00:13:27 -0700

    add xcode to shortcuts

--- osx.sh
+++ osx.sh
@@ -1,8 +1,9 @@
 # keyboard shortcuts
-defaults write com.apple.universalaccess com.apple.custommenu.apps -array "com.apple.Safari" "com.apple.Terminal" "com.google.Chrome"
+defaults write com.apple.universalaccess com.apple.custommenu.apps -array "com.apple.Safari" "com.apple.Terminal" "com.google.Chrome" "com.apple.dt.Xcode"
 defaults write com.apple.Safari NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i"
 defaults write com.apple.Terminal NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j"
 defaults write com.google.Chrome NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i"
+defaults write com.apple.dt.Xcode NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j" "Go Back" "@^o" "Go Forward" "@^i"
 
 # remove everything from dock
 defaults write com.apple.dock persistent-apps -array
