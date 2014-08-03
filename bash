16e768aba0a8a386c0d3e87797cd5f28eb129c90
sdc <sdc@ascension.local>
2014-08-02 19:12:03 -0700

    add fzf source, mpv, fix some keyboard shortcuts

--- bash
+++ bash
@@ -1,4 +1,9 @@
-
+# git
 alias gl="git log --pretty=format:'%C(yellow)%h %Cred%ar %Cblue%an%Cgreen%d %Creset%s'"
 alias gs="clear; git show"
 alias gd="clear; git status; git diff"
+
+# video
+alias mpv="/Applications/mpv.app/Contents/MacOS/mpv --af=scaletempo"
+
+source ~/.fzf.bash 2> /dev/null
--- osx.sh
+++ osx.sh
@@ -1,8 +1,5 @@
 # keyboard shortcuts
 defaults write com.apple.universalaccess com.apple.custommenu.apps -array "com.apple.Safari" "com.apple.Terminal" "com.google.Chrome"
-defaults write com.apple.Safari NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i"
-defaults write com.apple.Terminal NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j"
+defaults write com.apple.Safari NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i" "Show All Tabs" "@^p"
+defaults write com.apple.Terminal NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j"
 defaults write com.google.Chrome NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i"
-
-# remove everything from dock
-defaults write com.apple.dock persistent-apps -array
