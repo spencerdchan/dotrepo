7299ecb207a165f5cd4b5424330457d83ca70323
Spencer Chan <spencer@quora.com>
2015-02-02 20:06:17 -0800

    safari: hide all tabs

--- osx.sh
+++ osx.sh
@@ -1,7 +1,7 @@
 # keyboard shortcuts
 defaults write com.apple.universalaccess com.apple.custommenu.apps -array "com.apple.Safari" "com.apple.Terminal" "com.google.Chrome" "com.apple.dt.Xcode"
 
-defaults write com.apple.Safari NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i" "Show All Tabs" "@^p"
+defaults write com.apple.Safari NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i" "Show All Tabs" "@^p" "Hide All Tabs" "@^p"
 defaults write com.apple.Terminal NSUserKeyEquivalents -dict "Show Next Tab" "@^k" "Show Previous Tab" "@^j"
 defaults write com.google.Chrome NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j" "Back" "@^o" "Forward" "@^i"
 defaults write com.apple.dt.Xcode NSUserKeyEquivalents -dict "Select Next Tab" "@^k" "Select Previous Tab" "@^j" "Go Back" "@^o" "Go Forward" "@^i"
